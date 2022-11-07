5e85875af18dfd3cb90c09129abea13ec09ed6a0
Sanghyun124 <cloush24@gmail.com>
2022-11-07 17:59:27 +0900

    221107

---
+++ 22.11/221107/homework/src/App.vue
@@ -0,0 +1,43 @@
+// App.vue
+
+<template>
+  <div id="app">
+    <p>Counter: {{ $store.state.counter }}</p>
+    <p>Counter x2: {{ counterDouble }}</p>
+    <p></p>
+    <button @click="increase">increase +</button>
+    <button @click="decrease">decrease -</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App', 
+  // data: function() {
+  //   return {
+  //     counter: 0,
+  //   }
+  // },
+  methods: {
+    increase: function () {
+      // this.counter += 1
+      this.$store.dispatch('increase')
+    },
+    decrease: function () {
+      // this.counter -= 1
+      this.$store.dispatch('decrease')
+    },
+  },
+  computed: {
+    // counterDouble: function () {
+    //   return this.counter * 2
+    // },
+    counterDouble(){
+      return this.$store.getters.counterDouble
+    }
+  },
+}
+</script>
+
+<style>
+</style>
